c83d55431e6a8533696ac940c5fefdf9f24e76f9
Alex <eyamfruity@gmail.com>
2021-01-04 23:15:51 +0300

    почти садпатированная

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 //Плавный скролл
 $("a[href^='#']").click(function(){
-    var _href = $(this).attr("href");
+    let _href = $(this).attr("href");
     $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
     return false;
+});
+
+//Бургер
+let burger = $('.nav-burger');
+burger.on('click', function(){
+    burger.toggleClass('nav-burger_active');
+    $('.nav__list').fadeToggle();
 });
